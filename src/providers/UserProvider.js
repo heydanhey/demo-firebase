5038f4694c9635ba3f4f8d8e4626ce45dbcf9d4b
heydanhey <danhey@gmail.com>
2020-11-17 14:33:14 -0600

    update gitignore

--- src/providers/UserProvider.js
+++ src/providers/UserProvider.js
@@ -1,27 +1,27 @@
-import React, { Component, createContext } from "react";
-import { auth, generateUserDocument } from "../firebase";
+import React, { Component, createContext } from 'react'
+import { auth, generateUserDocument } from '../firebase'
 
-export const UserContext = createContext({ user: null, loading: false });
+export const UserContext = createContext({ user: null, loading: false })
 class UserProvider extends Component {
   state = {
     user: null,
-    loading: false
-  };
+    loading: false,
+  }
 
   componentDidMount = () => {
     this.setState({ loading: true })
-    auth.onAuthStateChanged(async userAuth => {
-      const user = await generateUserDocument(userAuth);
-      this.setState({ user, loading: false });
-    });
-  };
+    auth.onAuthStateChanged(async (userAuth) => {
+      const user = await generateUserDocument(userAuth)
+      this.setState({ user, loading: false })
+    })
+  }
 
   render() {
     return (
       <UserContext.Provider value={this.state}>
         {this.props.children}
       </UserContext.Provider>
-    );
+    )
   }
 }
-export default UserProvider;
+export default UserProvider
